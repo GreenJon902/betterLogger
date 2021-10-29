4f1b81b5c7cf2c22cf30b6aae271d52f36e3868b
GreenJon902 <GreenJon902@gmail.com>
2021-10-29 18:31:29 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='BetterLogger',
+    version='1.0.0',
+    description='A logging utility',
+    url='https://github.com/GreenJon902/BetterLogger',
+    author='GreenJon902',
+    packages=['BetterLogger'],
+    install_requires=['appdirs']
+)
