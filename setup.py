4586801b2c46a70f5ea1327857c98c487d3acb82
GreenJon902 <GreenJon902@gmail.com>
2021-10-29 19:47:35 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,31 @@
+import pathlib
 from setuptools import setup
 
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
 setup(
-    name='BetterLogger',
-    version='1.0.0',
-    description='A logging utility',
-    url='https://github.com/GreenJon902/BetterLogger',
-    author='GreenJon902',
-    packages=['BetterLogger'],
-    install_requires=['appdirs']
+    name="betterLogger",
+    version="1.0.0",
+    description="A great colored logger!",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GreenJon902/BetterLogger",
+    author="GreenJon",
+    author_email="GreenJon902@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.9"
+    ],
+    packages=["betterLogger"],
+    include_package_data=True,
+    install_requires=["appdirs"],
+    entry_points={
+        "console_scripts": [
+            "betterLogger=betterLogger.__init__:main",
+        ]
+    },
 )
