1dfc97c7dc28664f9ba1995dda50c82d8574cec2
GreenJon902 <GreenJon902@gmail.com>
2021-10-30 20:35:59 +0100

    updated version tag

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="betterLogger",
-    version="1.4",
+    version="1.5",
     description="A great colored logger!",
     long_description=README,
     long_description_content_type="text/markdown",
