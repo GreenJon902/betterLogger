24df8ff39222be5f6fb45c2c92aed3f6f7cc2473
GreenJon902 <GreenJon902@gmail.com>
2021-12-26 16:54:07 +0000

    made consoleHandler.py stream go through stdout

--- betterLogger/consoleHandler.py
+++ betterLogger/consoleHandler.py
@@ -1,8 +1,10 @@
+import sys
 from logging import StreamHandler as _StreamHandler
 
 
 class ConsoleHandler(_StreamHandler):
-    pass
+    def __init__(self):
+        _StreamHandler.__init__(self, stream=sys.stdout)
 
 
 __all__ = ["ConsoleHandler"]
